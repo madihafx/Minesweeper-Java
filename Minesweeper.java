fc53271dd57e50307f1ef5bab7ad256ceab68167
Your Name <you@example.com>
2026-04-14 14:28:50 -0400

    Added comments, created class, created main method.

---
+++ Minesweeper.java
@@ -0,0 +1,10 @@
+// Madiha, Hana, Laura, and Samia
+// Intro to CS Final Project
+// Minesweeper in Java
+// Spring 2026
+
+public class Minesweeper{
+    public static void main (String[] args){
+
+    }
+}
