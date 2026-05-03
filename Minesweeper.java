68a550f96d4a3d375a04dd84217fb98c86bf5e12
Laura Andrea Leguia <laurandrealeguia@gmail.com>
2026-05-03 16:38:44 -0400

    recursive reveal added

--- Minesweeper.java
+++ Minesweeper.java
@@ -64,16 +64,21 @@ public class Minesweeper{
 		//playing loop!
 		boolean gameOver = false;
 		while (gameOver == false) {	
+			
+			//ask for user input to reveal a cell
 			System.out.print("Enter row and column to reveal cell (only numbers 0-4, separated by a space):");
 	        Scanner scanner = new Scanner(System.in);
 			int r = scanner.nextInt();
 			int c = scanner.nextInt();
 			
-			revealed [r][c] = true;
-			
+			//explanation of reveal method is in comments within the method
+			reveal(matrix, revealed, r, c);
+	
 			//prints contents of the cell if it has been revealed, dashes if it hasn't
+			//this code generates the display the player sees, a new one is printed with each new input
 			for (int m = 0; m < 5; m++) {
 				for(int n = 0; n < 5; n++) {
+
 					if (revealed [m][n] == false) {
 						System.out.print("-" + " ");
 					}
@@ -83,6 +88,28 @@ public class Minesweeper{
 				}
 			//new line for each row	
 			System.out.println();
+			}			
+		}
+    }
+    public static void reveal (int[][] matrix, boolean[][]revealed, int r, int c) {
+    	
+    	//this line is the "base case", prevents infinite loop when checking neighbors,
+    	//and drops us back off in the playing loop if user enters coordinates for an already revealed cell
+    	if (revealed [r][c] == true) return;
+    	
+    	//if the cell wasn't already revealed, this line marks the chosen cell "revealed"
+    	revealed [r][c] = true;
+    	
+		//if the newly revealed cell is a 0, this code reveals all the neighbors
+		if (matrix [r][c] == 0 && revealed [r][c] == true) {
+			for (int row = Math.max(0,r-1); row <= Math.min(4, r+1); row++) {
+				for (int column = Math.max(0,c-1); column <= Math.min(4, c+1); column++) {
+					//this line calls the reveal function on itself, meaning:
+					//1) it checks if the neighbor we're looking has already been revealed, and if it has, goes to the next one
+					//2) if it hasn't been revealed, marks it as revealed
+					//3) if we're just marking it as revealed no and it's a 0, checks the neighbors
+					reveal(matrix, revealed, row, column); 
+				}
 			}
 		}
     }
